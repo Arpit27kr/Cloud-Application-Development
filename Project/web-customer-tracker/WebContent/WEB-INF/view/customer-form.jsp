0843c6f22a1a49800edfcdc5a3825a4f58e56c0e
Arpit27kr <arpit27kr@gmail.com>
2023-03-21 23:58:02 +0530

    committing database connection with spring and creating functionalities

---
+++ Project/web-customer-tracker/WebContent/WEB-INF/view/customer-form.jsp
@@ -0,0 +1,19 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+</head>
+
+<body>
+
+
+Customer data 
+
+</body>
+
+
+</html>
---
+++ Project/web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -0,0 +1,64 @@
+package com.luv2code.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	//need to inject the dao inside the controller
+	
+	//@Autowired // spring will scan for component that implements customerdao interface
+//	private CustomerDAO customerDAO;
+	
+	
+	
+	//injecting the customerservice 
+	@Autowired
+	 private CustomerService customerService;
+	
+	
+	
+	
+	@RequestMapping("/list")
+	public String listCustomer(Model theModel)
+	{
+		
+		List<Customer> theCustomers=customerService.getCustomers(); //get customer from service
+		
+		theModel.addAttribute("customers", theCustomers); // add customer inside the model 
+		
+		 
+		
+		
+		
+		return "list-customer";
+	}
+	
+	
+	
+	@GetMapping("/showFormForAdd")
+	public String showForForAdd(Model theModel)
+	{
+		return "customer-form";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
